c7b4f39255b582c233b4c20c9d46e7bee85cbd0d
JairTb <jdtb151@gmail.com>
2024-06-11 11:05:45 -0500

    newR

--- sc.js
+++ sc.js
@@ -12,4 +12,47 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+const form = document.getElementById('contactForm');
+const parrafo = document.getElementById('warnings');
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let warnings="";
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const subjectInput = document.getElementById('subject');
+    const messageInput = document.getElementById('message');
+
+    let entrar = false;
+    parrafo.innerHTML = '';
+
+    if (nameInput.value.length < 6) {
+        warnings += 'El nombre no es válido <br>';
+        entrar = true;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailInput.value)) {
+        warnings += 'El email no es válido <br>';
+        entrar = true;
+    }
+
+    if (subjectInput.value.length < 6) {
+        warnings += 'El asunto no es válido <br>';
+        entrar = true;
+    }
+
+    if (messageInput.value.length < 9) {
+        warnings += 'El mensaje no es válido <br>';
+        entrar = true;
+    }
+
+    if (entrar) {
+        parrafo.innerHTML = warnings
+    } else {
+        parrafo.innerHTML = "enviado"
+    }
+    return false;
+});
+
 
